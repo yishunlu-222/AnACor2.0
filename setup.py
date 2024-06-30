6c9f4a165fb7f7e42ceeb13ef300326a37c634c8
yishunlu-222 <790585415@qq.com>
2024-06-30 22:35:38 +0100

    added setup.py

--- setup.py
+++ setup.py
@@ -1,45 +1,67 @@
 from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
 import subprocess
+import os
+
+def get_gpu_model():
+    try:
+        result = subprocess.run(['nvidia-smi', '-L'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        if result.returncode != 0:
+            raise RuntimeError("nvidia-smi command failed")
+
+        # Example output: GPU 0: Tesla V100-SXM2-16GB (UUID: GPU-...)
+        match = re.search(r'GPU \d+: ([\w\s-]+)', result.stdout)
+        if match:
+            return match.group(1).strip().lower().replace(' ', '')
+        else:
+            raise RuntimeError("Could not parse nvidia-smi output")
+    except Exception as e:
+        print(f"Error detecting GPU model: {e}")
+        return None
+
 class CustomBuild(build_ext):
     def run(self):
-        # run Makefile or other build commands
-        subprocess.check_call(['cd', './AnACor/src'])
-        subprocess.check_call(['make', 'cpu'])
+        gpu_model = get_gpu_model()
+        if not gpu_model:
+            raise RuntimeError("Failed to detect GPU model")
+
+        # Change to the directory containing the Makefile
+        os.chdir('./AnACor/src')
+        # Run the make command with the detected GPU model
+        subprocess.check_call(['make', 'cpu', f'ARCH={gpu_model}'])
+        # Return to the original directory
+        os.chdir('../../')
         super().run()
 
 setup(
     name='AnACor',
-    version='1.2',
+    version='2.0',
     packages=find_packages(),
-        cmdclass={
+    cmdclass={
         'build_ext': CustomBuild,
     },
-    description='AnACor for analytical absorption correction by tomography reconstruction',
+    description='AnACor2.0 for analytical absorption correction by tomography reconstruction',
     author='Yishun Lu',
     author_email='yishun.lu@eng.ox.ac.uk, wes.armour@oerc.ox.ac.uk',
+    entry_points={
+        'console_scripts': [
+            'anacor.preprocess = AnACor.preprocess_lite:main',
 
-
-    entry_points = {
-        'console_scripts' :
-            ['anacor.preprocess = AnACor.preprocess_lite:main',
-             'anacor.main = AnACor.main_lite:main',
             #  'anacor.postprocess = AnACor.postprocess:main',
-             'anacor.preprocess_lite = AnACor.preprocess_lite:main',
-             'anacor.main_lite = AnACor.main_lite:main',
-             'anacor.mp_lite = AnACor.mp_lite:main',
+            'anacor.preprocess_lite = AnACor.preprocess_lite:main',
+            'anacor.main = AnACor.main_lite:main',
+            'anacor.main_lite = AnACor.main_lite:main',
+            'anacor.mp = AnACor.mp_lite:main',
+            'anacor.mp_lite = AnACor.mp_lite:main',
             #  'anacor.postprocess_lite = AnACor.postprocess_lite:main',
-             'anacor.init = AnACor.initialization:main' ,
-             ],
+            'anacor.init = AnACor.initialization:main',
+        ],
     },
-
     install_requires=[
         'importlib-metadata; python_version == "3.8"',
         'opencv-python>=4.6.0',
         'scikit-image<=0.19.0',
         'numba',
         'imagecodecs',
-
-
-    ],)
-            #'dials @ https://github.com/yishunlu-222/dials_precalcu/tree/precalc_abs_model',
+    ],
+)
